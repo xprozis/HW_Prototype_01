fbab8769d7177995189bd226fe93d3454b338a53
joaoantonio <joaoantonio.ferreira@prozis.com>
2024-12-18 13:38:36 +0000

    Melhoramenos na pagina e navegacao

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import streamlit as st
+from PIL import Image
+
+prozis_icon = Image.open("./images/p_logo.ico")
 
 pages = {
-    "Configurações": [
-        st.Page("page_1.py", title="Login", icon="🔥"),
-        st.Page("page_2.py", title="Pagina 2", icon="🔥"),
-        st.Page("page_3.py", title="Pagina 3", icon="🔥"),
+    "Gestor de Stock": [
+        st.Page("page_1.py", title="Pagina 1", icon="🌐"),
+        st.Page("page_2.py", title="Pagina 2", icon="⚙️"),
+        st.Page("page_3.py", title="Pagina 3", icon="❔"),
     ],
 }
 
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_icon=prozis_icon,layout="wide")
 pg = st.navigation(pages)
 pg.run()
--- page_1.py
+++ page_1.py
@@ -1,22 +1,25 @@
 import streamlit as st
 import pandas as pd
 from controller.page_1_c import *
-import time
 
 df = pd.DataFrame(["No data"])
 
 st.header("Pagina 1")
 st.divider()
 
-query_exemplo = """
-SELECT * FROM assembly.program
+query_exemplo = """SELECT * FROM assembly.program
 ORDER BY id ASC """
 
 
 query = st.text_area("Inserir a query desejada", value = query_exemplo)
+conn = st.connection("postgresql", type="sql") 
+df = conn.query(query, ttl="10m") 
+
 
 if st.button("Enviar Query"):
   conn = st.connection("postgresql", type="sql") 
   df = conn.query(query, ttl="10m") 
-  st.data_editor(df)
 
+
+if not df.empty:
+  st.data_editor(df, use_container_width=True)
