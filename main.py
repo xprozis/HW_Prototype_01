75329864d96954b1a0a0fdca05588efe08b48c8a
joaoantonio <joaoantonio.ferreira@prozis.com>
2025-01-03 14:53:52 +0000

    Login_PAGE

--- main.py
+++ main.py
@@ -7,26 +7,25 @@ if 'user_id' not in ss:
 
 pages = {
     "Gestão de Componentes": [
-        st.Page("page_1.py", title="Pesquisar", icon="🌐"),
-        st.Page("page_2.py", title="Adicionar", icon="⚙️"),
+        st.Page("page_1.py", title="Pesquisar", icon="🔎"),
+        st.Page("page_2.py", title="Adicionar", icon="➕"),
     ],
     "Gestão de Cotações": [
-        st.Page("page_3.py", title="Pesquisar", icon="🌐"),
-        st.Page("page_4.py", title="Adicionar", icon="⚙️"),
+        st.Page("page_3.py", title="Pesquisar", icon="🔎"),
+        st.Page("page_4.py", title="Adicionar", icon="➕"),
     ],
 }
 
 st.set_page_config(page_icon=Image.open("./images/p_logo.ico"),layout="wide")
 
 if ss.user_id is None:
-    col1,col2,col3 = st.columns(3)
+    col1,col2,col3 = st.columns([2,1,2])
     with col2:
-        st.subheader("Página de login")
+        st.subheader("Login")
+        user_input = st.text_input("Username", max_chars=10)
+        password_input = st.text_input("Password", type="password", max_chars=4)
         st.divider()
-        user_input = st.text_input("Username")
-        password_input = st.text_input("Password", type="password")
-        st.divider()
-        if st.button("LOGIN", use_container_width=True, type="primary", key = "botao_login"):
+        if st.button("Entrar", use_container_width=True, type="primary", key = "botao_login"):
             # Vai ler da BD a tabela do login e vai comparar se existe na tabela. Se sim, muda uma flag para nao fazer login de novo.
             if ss.botao_login:
                 ss.user_id = user_input
